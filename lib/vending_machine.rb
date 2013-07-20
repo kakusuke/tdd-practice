a3e9fcc4e6ce59511a96e7618c449f2d3f1a5cee
kakusuke <bananafishmonger+github@gmail.com>
2013-07-20 14:44:58 +0900

    step-1

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -1,10 +1,14 @@
 class VendingMachine
 	def put(coin)
-		coins.push(coin)
+		if [10, 50, 100, 500, 1000].include? coin then
+			coins.push(coin)
+			coin = 0
+		end
+		coin
 	end
 
 	def amounts
-		coins.reduce {|sum, coin| sum + coin}
+		coins.reduce(0) {|sum, coin| sum + coin}
 	end
 
 	def cancel
--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -5,6 +5,21 @@ describe VendingMachine do
 		@vending_machine = VendingMachine.new
 	end
 
+	describe "#put" do
+		[10, 50, 100, 500, 1000].each do |coin|
+			it "should not return coin" do
+				expect(@vending_machine.put coin).to eq 0
+				expect(@vending_machine.amounts).to eq coin
+			end
+		end
+		[1, 5, 2000, 5000, 10000].each do |coin|
+			it "should return coin" do
+				expect(@vending_machine.put coin).to eq coin
+				expect(@vending_machine.amounts).to eq 0
+			end
+		end
+	end
+
 	describe "#amounts" do
 		[10, 50, 100, 500, 1000].each do |coin|
 			it "should return 30 when put #{coin} yen 3 times" do
